57489b193eb27d6b10e00878bca56716e3cf2c1f
spyroot <spyroot@gmail.com>
2024-02-16 02:08:28 +0400

    inittial

--- main.py
+++ main.py
@@ -6,63 +6,27 @@
 #  - packet per second for network, latency , storage IO etc.
 #
 #
+import json
 import subprocess
+from pathlib import Path
 from typing import List
 
 import paramiko
 from os.path import expanduser
 import argparse
 
-from nodes import KubernetesState
+from kube_state import KubernetesState
+from node_actions import NodeActions
+from ssh_runner import SshRunner
 
 
-# NODE_POOL_NAME = "vf-test-np1-"
-# DEFAULT_UPLINK = "eth0"
-# TUNED_PROFILE_NAME = "mus"
-# TUNED_PROFILE_PATH = f"/usr/lib/tuned/{TUNED_PROFILE_NAME}/tuned.conf"
-# setting_keys = ["default_hugepagesz", "hugepages", "intel_idle.max_cstate"]
-# setting_values = ["1G", "16", "0"]
-#
-# node_ips = ["192.168.1.1", "192.168.1.2"]
-#
-#
-
-
-#
-# def update_ring_buffer(adapter_name: str, node_ips: list, tx_value, rx_value):
-#     """Function update on remote host ring buffer
-#     :return:
-#     """
-#     for ip in node_ips:
-#         ssh_command(
-#             ip, f"sudo ethtool -G adapter_name rx {tx_value} tx {tx_value}",
-#             password="your_password")
-#
-#
-# def update_tuned(tuned_profile_name: str, node_ips: list):
-#     """Function update on remote host tuned profile
-#     """
-#     for ip in node_ips:
-#         profile_name = ssh_command(ip, "sudo tuned-adm active | awk '{print $NF}'", password="your_password")
-#         print(f"Active profile on {ip}: {profile_name}")
-#         if profile_name != tuned_profile_name:
-#             print(f"Setting profile tuned_profile_name on {ip}...")
-#             ssh_command(ip, f"sudo tuned-adm profile {TUNED_PROFILE_NAME}", password="your_password")
-#             ssh_command(ip, "sudo reboot", password="your_password")
-#         else:
-#             print(f"Profile on {ip} is already set to {TUNED_PROFILE_NAME}. No changes made.")
-#
-
-def main(args):
+def main(cmd_args):
     """
 
     :return:
     """
-    public_key_path = f"{expanduser('~')}/.ssh/id_rsa.pub"
-    tuned_profile = f"/usr/lib/tuned/{args.tuned_profile_name}/tuned.conf"
     kube_state = KubernetesState()
-    nodes = kube_state.fetch_nodes_uuid_ip(
-        args.node_pool_name)
+    nodes = kube_state.fetch_nodes_uuid_ip(args.node_pool_name)
 
     print(kube_state.node_names())
     print(kube_state.pod_node_ns_names(ns="all"))
@@ -70,15 +34,24 @@ def main(args):
     print(kube_state.read_kube_config())
     print(kube_state.read_cluster_name())
 
-    # print("Node IPs:", node_ips)
+    print(cmd_args.test_spec)
+    with open(cmd_args.test_spec, 'r') as file:
+        test_spec = json.load(file)
+        server_config = test_spec.get('environment', {}).get('server', {})
+        client_config = test_spec.get('environment', {}).get('client', {})
+        server_pod_spec = Path(server_config.get('pod_spec')).resolve().absolute()
+        client_pod_spec = Path(client_config.get('pod_spec')).resolve().absolute()
+        node_output = KubernetesState.run_command(f"kubectl apply -f {server_pod_spec}")
+        node_output = KubernetesState.run_command(f"kubectl apply -f {client_pod_spec}")
+
     #
-    # push_keys(node_ips, public_key_path, args.username, args.password)
-    # update_ring_buffer(node_ips, args.default_uplink, args.username, args.password)
-    # update_tuned(node_ips, args.tuned_profile_name, f"/usr/lib/tuned/{args.tuned_profile_name}/tuned.conf",
-    #              args.username, args.password)
+    # ssh_runner = SshRunner(kube_state.node_ips(), username="capv", password="VMware1!")
+    # node_actions = NodeActions(kube_state.node_ips(), ssh_runner)
+    #
+    # node_actions.update_ring_buffer()
+    # node_actions.update_active_tuned()
+    # node_actions.run_iperf_test()
 
-
-# # Modify tuned profile configurations as needed...
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Node configuration script.")
@@ -87,5 +60,7 @@ if __name__ == '__main__':
     parser.add_argument("--tuned-profile-name", default="mus", help="Tuned profile name.")
     parser.add_argument("--username", default="capv", help="Username for SSH.")
     parser.add_argument("--password", help="Password for SSH (optional).")
+    parser.add_argument("--test_spec", default="mutate.json", help="test scenarion")
+
     args = parser.parse_args()
     main(args)
--- ssh_runner.py
+++ ssh_runner.py
@@ -4,27 +4,68 @@ from pathlib import Path
 from typing import List
 import paramiko
 from os.path import expanduser
+import time
 
 
 class SshRunner:
     def __init__(
             self,
             remote_hosts: List[str],
-            username: str,
-            password=None
+            username: str = "capv",
+            password=None,
+            public_key_path=None,
     ):
         """
         :param username:
         :param password:
         """
-        self.remote_hosts = remote_hosts
+        self._remote_hosts = remote_hosts
         self.username = username
         self.password = password
         self._is_pubkey_authenticated = False
 
         # default path
         self.public_key_path = f"{expanduser('~')}/.ssh/id_rsa.pub"
+        if public_key_path is not None:
+            self.public_key_path = public_key_path
+
         self.initial_auth()
+        self.persistent_connections = {}
+
+    def __enter__(self):
+        """
+        Return the instance itself when entering the context.
+        """
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """
+        Ensure all connections are closed when exiting the context.
+        """
+        print("closing connections")
+        self.close_all_connections()
+
+    def close_all_connections(self):
+        """Close all connections to remote host
+        :return:
+        """
+        for ip, client in self.persistent_connections.items():
+            client.close()
+        self.persistent_connections.clear()
+
+    def get_ssh_connection(self, ip):
+        """
+        :param ip:
+        :return:
+        """
+        if ip not in self.persistent_connections:
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.load_system_host_keys()
+            client.connect(ip, username=self.username, password=self.password)
+            self.persistent_connections[ip] = client
+
+        return self.persistent_connections[ip]
 
     def run(self, ip, command):
         """Run cmd on remote host
@@ -32,14 +73,20 @@ class SshRunner:
         :param command:
         :return:
         """
-        client = paramiko.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.load_system_host_keys()
-        client.connect(ip, username=self.username, password=self.password)
+        client = self.get_ssh_connection(ip)
+
+        print("executing command {} on a host {}".format(command, ip))
+        start_time = time.time()
         stdin, stdout, stderr = client.exec_command(command)
+        # This blocks until the command finishes
+        stdout.channel.recv_exit_status()
+
+        end_time = time.time()
         output = stdout.read().decode('utf-8').strip()
-        client.close()
-        return output
+        execution_time = end_time - start_time
+        exit_code = stdout.channel.exit_status
+
+        return output, exit_code, execution_time
 
     def initial_auth(self):
         """Initial authentication
@@ -51,13 +98,19 @@ class SshRunner:
         else:
             self._is_pubkey_authenticated = True
 
-    def run_local_command(self, cmd):
-        """Run local command
+    @staticmethod
+    def run_command(cmd):
+        """
         :param cmd:
         :return:
         """
-        result = subprocess.run(cmd, shell=True, text=True, capture_output=True)
-        return result.stdout.strip()
+        result = subprocess.run(
+            cmd, capture_output=True, text=True, shell=True
+        )
+        if result.returncode == 0:
+            return result.stdout.strip().split('\n')
+        else:
+            raise Exception(f"Command '{cmd}' failed with error: {result.stderr.strip()}")
 
     # def check_key(self, node_ips, public_key_path, username, password=None):
     #     """Function to push SSH keys to remote hosts , all follow ssh command executed
@@ -85,24 +138,35 @@ class SshRunner:
     #             ssh_command(ip, append_command, username, password)
     #             print(f"SSH key successfully copied to {ip}.")
 
-    def push_keys(self, nodes: List[str]):
+    def push_keys(self):
         """For initial connection we copy ssh key to remote nodes by leveraging
         ssh-copy-id all follow up ssh connection will use key authentication.
         Note it assume server accept that.
 
-        :param nodes:
         :return:
         """
-
         pub_key_path = Path(self.public_key_path)
         if not pub_key_path.exists():
-            print("Found not found {}".format(pub_key_path))
+            print("Not found {}".format(pub_key_path))
             print("Please generate public key first. run ssh-keygen")
             return
 
         with open(self.public_key_path, "r") as file:
             public_key = file.read().strip()
 
-            for ip in nodes:
+            for ip in self._remote_hosts:
                 print(f"Copying SSH key to {ip}...")
                 subprocess.run(["ssh-copy-id", f"{self.username}@{ip}"])
+
+    def release_connection(self, ip):
+        """
+        Closes the SSH connection for a specific host
+        and removes it from the persistent connections.
+
+        :param ip: IP address of the host whose connection should be released.
+        """
+        # Check if the connection exists for the given IP
+        if ip in self.persistent_connections:
+            self.persistent_connections[ip].close()
+            del self.persistent_connections[ip]
+
